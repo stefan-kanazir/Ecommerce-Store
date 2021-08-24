fefe7d9a363da171fbdfde57be8e806a21d86d53
stefan-kanazir <kanazir.stefan@gmail.com>
2021-08-24 22:39:28 +0200

    Cart page setup and style

--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -3,12 +3,12 @@ import CartItem from "./CartItem";
 
 export default function CartList({value}) {
     const {cart} = value;
-    console.log(value);
 
     return (
         <div className="container-fluid">
-            Hello from cart list
-            <CartItem></CartItem>
+            {cart.map(item => {
+                return <CartItem key={item.id} item={item} value={value} />
+            })}
         </div>
     )
 }
