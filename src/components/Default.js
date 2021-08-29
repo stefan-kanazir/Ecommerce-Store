6a9789fa646514a4ba5aedb4e9baddb921ba0d26
stefan-kanazir <kanazir.stefan@gmail.com>
2021-08-29 23:25:27 +0200

    404 error page

--- src/components/Default.js
+++ src/components/Default.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react'
 
 export default class Default extends Component {
     render() {
+        console.log(this.props);
         return (
-            <div>
-                <h3>Page not found</h3>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto text-center text-uppercase pt-5">
+                        <h1 className="display-3">404 error</h1>
+                        <h3 className="mb-5">Page not found</h3>
+                        <p>Tech requested URL &nbsp;
+                            <span className="text-danger">{this.props.location.pathname}</span>{""}
+                            &nbsp; was not found
+                        </p>
+                    </div>
+                </div>
             </div>
         )
     }
